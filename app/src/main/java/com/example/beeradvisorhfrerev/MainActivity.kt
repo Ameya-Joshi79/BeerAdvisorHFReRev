f64f3bf9bdd414e1a3e5d1e047816ec9be25e4e0
Ameya-Joshi79 <45701553+Ameya-Joshi79@users.noreply.github.com>
2024-05-09 14:09:14 -0400

    Basic Setup of App
    Version.1
    1. Added string resources to strings.xml
    2. Added some Views to the LinearLayout

---
+++ app/src/main/java/com/example/beeradvisorhfrerev/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.beeradvisorhfrerev
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.beeradvisorhfrerev.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
