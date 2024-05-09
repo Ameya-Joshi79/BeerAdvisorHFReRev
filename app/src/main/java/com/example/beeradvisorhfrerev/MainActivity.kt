9d4afe2344950d2c3e2b569064c19f89f8c33e71
Ameya-Joshi79 <45701553+Ameya-Joshi79@users.noreply.github.com>
2024-05-09 16:11:46 -0400

    MainActivity.kt
    1. App now displays a List of Beer Names based on the Value selected in the Spinner
    2. We're using Kotlin's Higher Order function(s) to concatenate the String values from the List of Beers and finally displaying them on the MainActivity

--- app/src/main/java/com/example/beeradvisorhfrerev/MainActivity.kt
+++ app/src/main/java/com/example/beeradvisorhfrerev/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.beeradvisorhfrerev
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
@@ -31,12 +32,41 @@ class MainActivity : AppCompatActivity() {
 
             val selectedColor = beerSpinner.selectedItem.toString()
 
-            beerSuggestionsTextView.text = "Brand Color is ${selectedColor}"
+            //beerSuggestionsTextView.text = "Brand Color is ${selectedColor}"
+
+            val beerSuggestionsList = getBeerList(selectedColor)
+
+            val beerSuggestions = beerSuggestionsList.fold(""){ str, item ->
+                if (str.isEmpty()) "$item" else "$str, $item"
+            }
+
+            Log.d("BEERADVISERAPP", beerSuggestions)
+
+            beerSuggestionsTextView.text = beerSuggestions
+
+            val beerSuggestionsUsingReduce = beerSuggestionsList.reduce{str, item ->
+                str + '\n' + item
+            }
+
+            Log.d("BEERADVISERAPP", beerSuggestionsUsingReduce)
 
         }//Button.setOnClickListener
 
 
     }//onCreate
+
+
+    fun getBeerList(selectedBeerColor:String):List<String>{
+
+        return when(selectedBeerColor){
+
+            "Light" -> listOf("Jail Pale Ale", "Lager Light")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+             else -> listOf("Gout Stout", "Dark Daniel")
+        }//when
+
+    }//getBeetList
 
 
 }
